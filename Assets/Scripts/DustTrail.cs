0b7fd00c2efd3dc6487c20cad248f47a630e2827
BatemaDevelopment <lukasbatema@mg.batemadevelopment.com>
2023-03-10 21:36:24 -0500

    g (#3)

---
+++ Assets/Scripts/DustTrail.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+    [SerializeField] ParticleSystem dustTrailEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.collider.tag == "Ground" && collision.otherCollider.tag == "Player Body") && !(collision.collider.tag == "Clouds" && collision.otherCollider.tag == "Player Body"))
+        {
+            Debug.Log("You told your level 100 Bulbasaur to use explosion, but instead it used explosion!"); // niche Pokémon reference to a glitch in Pokémon Green (not Leaf Green) Pokémon ;)
+
+            dustTrailEffect.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+         Debug.Log("Your level 100 Bulbasaur used explosion!"); // niche Pokémon reference to a glitch in Pokémon Green (not Leaf Green) Pokémon ;)
+
+         dustTrailEffect.Stop();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,21 +5,69 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float torqueAmount = 1f;
     [SerializeField] float torqueModifier = 0.75f;
 
+    float originalSpeed = 15f;
+    float finalSpeed = 0f;
+    [SerializeField] float speedUp = 35f;
+    [SerializeField] float slowDown = 5f;
+
     Rigidbody2D rb2d;
+    SurfaceEffector2D effector2D;
+
+    bool fatesCollided;
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        effector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((collision.collider.tag == "Ground" && collision.otherCollider.tag == "Player Head") && !(collision.collider.tag == "Clouds" && collision.otherCollider.tag == "Player Head"))
+        {
+            fatesCollided = true;
+        }
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && !fatesCollided)
+        {
+            effector2D.speed = speedUp;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) && !fatesCollided)
+        {
+            effector2D.speed = slowDown;
+        }
+        else if ((Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow)) && !fatesCollided)
+        {
+            effector2D.speed = originalSpeed;
+        }
+        else if (fatesCollided)
+        {
+            effector2D.speed = finalSpeed;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) && !fatesCollided)
         {
             rb2d.AddTorque(torqueAmount * torqueModifier);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow) && !fatesCollided)
         {
             rb2d.AddTorque(-torqueAmount);
         }
+        else if (fatesCollided)
+        {
+            rb2d.AddTorque(0f);
+         }
     }
 }
